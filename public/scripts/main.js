1c5606cbd7a6dab1489fd3b3da0e9ad2adddd9ae
Daryl Amodan <darylamodan@gmail.com>
2018-04-03 09:03:49 -0400

    initial commit

---
+++ public/scripts/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//JS to activate Hamburger Menu
+function openSideMenu() {
+    document.getElementById('side-menu').style.width = '75%';
+}
+function closeSideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+}
